0227086f89c14267e38955ae1b9008cb68eab15c
clchauvin <clchauvin@ITEM-S77372.emea.msad.sopra>
2020-10-22 14:49:11 +0200

    [Config Maven] Mise en marche controller, creation jobLauncher execute

--- src/main/java/com/sandbox/BatchController.java
+++ src/main/java/com/sandbox/BatchController.java
@@ -1,17 +1,13 @@
 package com.sandbox;
 
-import java.util.Objects;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
+@RestController
 public class BatchController {
-	@RequestMapping("/hello")
+	@GetMapping("/hello")
 	String hello() {
-		return "Hello";
+		return "Hello World";
 	}
 
 }
--- src/main/java/com/sandbox/JobLauncherMain.java
+++ src/main/java/com/sandbox/JobLauncherMain.java
@@ -1,8 +1,38 @@
 package com.sandbox;
 
-import org.springframework.context.annotation.EnableMBeanExport;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import java.util.Date;
+import java.util.Objects;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+
+@EnableBatchProcessing
+@Component
 public class JobLauncherMain {
+
+	@Autowired
+	private ApplicationContext context;
+	@Autowired
+	JobLauncher jobLauncher;
+
+	public void execute(String jobName, JobParameters jps) throws JobExecutionAlreadyRunningException,
+			JobRestartException, JobInstanceAlreadyCompleteException, JobParametersInvalidException {
+		Job job = (Job) context.getBean(jobName);
+		if (Objects.nonNull(jobName)) {
+			JobParameters defautJps = Objects.nonNull(jps) ? jps
+					: new JobParametersBuilder().addDate("TIME", new Date()).toJobParameters();
+			jobLauncher.run(job, defautJps);
+		}
+	}
 
 }
